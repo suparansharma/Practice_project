8642b73539b3a7a8327330ecf174b69e28d785d0
suparansharma <suparansharmap@gmail.com>
2022-03-07 00:57:43 +0600

    increment function done

--- mobile-shopping-cart-main/app.js
+++ mobile-shopping-cart-main/app.js
@@ -1,1 +1,25 @@
-console.log('I am in a separate file')
+//mobile items
+
+//+ Button
+var mobilePlusBtn = document.getElementById("mobilePlusBtn");
+mobilePlusBtn.addEventListener("click", function () {
+  var increaseMobileNumber = document.getElementById("MobileNumber");
+  var increaseMobileInNumber = parseFloat(increaseMobileNumber.value);
+  var newIncreaseMobileNumber = increaseMobileInNumber + 1;
+  increaseMobileNumber.value = newIncreaseMobileNumber;
+
+  var mobilePrice = 1219 * newIncreaseMobileNumber;
+
+  document.getElementById("mobilePrice").innerText = mobilePrice;
+
+  console.log(mobilePrice);
+});
+
+//- button
+// var mobileMinusBtn = document.getElementById("mobileMinusBtn");
+// mobileMinusBtn.addEventListener("click", function () {
+//   var decreaseMobileNumber = document.getElementById("MobileNumber");
+//   var decreaseMobileInNumber = parseFloat(decreaseMobileNumber);
+//   var newDecreaseMobileNumber = decreaseMobileInNumber - 1;
+//   decreaseMobileNumber.value = newDecreaseMobileNumber;
+// });
