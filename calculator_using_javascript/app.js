48eaef977d07ba9db8ebe2e9e3fbe32d7fa195b9
suparansharma <suparansharmap@gmail.com>
2022-03-08 10:22:48 +0600

    complete

---
+++ calculator_using_javascript/app.js
@@ -0,0 +1,8 @@
+function calculate(x) {
+  form.display.value = form.display.value + x;
+}
+
+function clearFunction() {
+  document.getElementById("inputBox").innerText = "";
+  console.log("button click");
+}
