2af6638ff7a7866958ee1dbcc00a2f14c0b40c91
suparansharma <suparansharmap@gmail.com>
2022-05-12 01:11:51 +0600

    firebase initilized

--- fire-aurth/src/App.js
+++ fire-aurth/src/App.js
@@ -1,29 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
 import * as firebase from 'firebase/app';
-import 'firebase/auth';
+import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth,signInWithPopup,GoogleAuthProvider  } from "firebase/auth";
 import firebaseConfig from './firebase.config';
-firebase.initializeApp(firebaseConfig);
+// import {getAuth, signInWithPopup, GoogleAuthProvider } from  'firebase/auth';
+// import firebaseConfig from './firebase.config';
+// firebase.initializeApp(firebaseConfig);
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 function App() {
+
+  
+  //    const provider = new GoogleAuthProvider();
+  //   //  const provider = new firebase.auth.GoogleAuthProvider();
+  //   //  const auth = getAuth();
+  //    const handleSignIn =()=>{
+  //   firebase.auth.signInWithPopup(provider)
+  //   .then(res =>{
+  //     console.log(res);
+  //   })
+
+
+  // }
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const provider = new GoogleAuthProvider();
+ const handleSignIn =()=>{
+  signInWithPopup(auth, provider)
+  .then(res => {
+    const {displayName,email,photoURL}=res.user;
+    console.log(displayName,email,photoURL);
+  })
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+     <button onClick={handleSignIn} >SignIn</button>
+   </div>
   );
 }
 
