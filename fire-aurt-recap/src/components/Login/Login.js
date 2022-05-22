543eea792d4b2f0f92d52ae55760b5e429a41b65
suparansharma <suparansharmap@gmail.com>
2022-05-23 01:41:01 +0600

    google popupb

--- fire-aurt-recap/src/components/Login/Login.js
+++ fire-aurt-recap/src/components/Login/Login.js
@@ -1,10 +1,40 @@
 import React, { useContext } from 'react';
-
+import { initializeApp } from "firebase/app";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebaseConfig from './firebase.config';
 
 const Login = () => {
+    const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+const handleGoogleSignIn = ()=>{
+    const auth = getAuth();
+    const Googleprovider = new GoogleAuthProvider();
+signInWithPopup(auth, Googleprovider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+
+}
     return (
         <div>
             <h1>This is Login</h1>
+            <button onClick={handleGoogleSignIn}>Login By Google</button>
         </div>
     );
 };
---
+++ fire-aurt-recap/src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDpH_EW_fuPYUonD_1OaHrscN04s3qVFNM",
+    authDomain: "fire-aurth-recap.firebaseapp.com",
+    projectId: "fire-aurth-recap",
+    storageBucket: "fire-aurth-recap.appspot.com",
+    messagingSenderId: "642415925855",
+    appId: "1:642415925855:web:693723821c60ffdcc119ea"
+  };
+
+export default firebaseConfig;
