11bf00eaa502e3ba28984e0d8e03c4f7cf2e28b6
suparansharma <suparansharmap@gmail.com>
2022-06-27 01:36:12 +0600

    get and post method

--- backend/node_mongo/index.js
+++ backend/node_mongo/index.js
@@ -1,7 +1,62 @@
+// const express = require('express');
+// const cors = require('cors');
+
+// const app = express();
+
+// app.use(cors());
+
+
+// const users = ["Asad","Moin","Saheb","Susmita","Rohan","Rahim"];
+
+// app.get('/users/:id',(req,res) =>{
+//     const id =req.params.id;
+//     const name = users[id];
+//     res.send({id,name});
+// })
+
+// app.get('/',(req,res)=>{
+//     const fruit ={
+//         product:"ada",
+//         price:220
+//     }
+//     res.send(fruit);
+// })
+
+// app.get('/fruit/banana',(req,res)=>{
+//     const banana ={
+//         name:'banana',
+//         product:"ada",
+//         price:220,
+//         quantity:100
+//     }
+//     res.send(banana);
+// })
+
+// app.listen(3000,()=> console.log("listenig port 3000"));
+
 const express = require('express');
+const cors = require('cors');
 const app = express();
-app.get('/',(req,res)=>{
-    res.send("thank you for calling me");
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.post('/appUser',(req,res) =>{
+    // res.end(JSON.stringify(req.body))  
+    // res.send(req.body) 
+    console.log("data received",req.body)
 })
 
+
+const users = ["Asad","Moin","Saheb","Susmita","Rohan","Rahim"];
+
+app.get('/users/:id',(req,res) =>{
+    const id =req.params.id;
+    const name = users[id];
+    res.send({id,name});
+})
 app.listen(3000,()=> console.log("listenig port 3000"));
+
+
+
