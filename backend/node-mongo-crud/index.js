836c5a22fd29993b4687b49178989d722f6239cc
suparansharma <suparansharmap@gmail.com>
2022-07-18 00:04:42 +0600

    all

--- backend/node-mongo-crud/index.js
+++ backend/node-mongo-crud/index.js
@@ -1,24 +1,39 @@
-const express = require ('express')
+const express = require("express");
+const bodyParser = require("body-parser");
 const MongoClient = require("mongodb").MongoClient;
 // const { MongoClient, ServerApiVersion } = require('mongodb');
-const password = 'admin';
-const uri = "mongodb+srv://admin:admin@cluster0.hnwppqy.mongodb.net/organicdb?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const password = "organicPassword";
+// const uri = "mongodb+srv://admin:organicPassword@cluster0.hnwppqy.mongodb.net/organicdb?retryWrites=true&w=majority";
+const uri =
+  "mongodb+srv://organicUser:organicPassword@cluster0.awoh24t.mongodb.net/organicdb?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const app = express();
-app.get('/',(req,res)=>{
-    res.send('hello i am working')
-})
-
-client.connect(err => {
-  const collection = client.db("organicdb").collection("products");
-  const product = {name:"modhu",price:25, quantity:20 }
-  collection.insertOne(product)
-  .then(result =>{
-console.log("one product added")
-  })
-  // perform actions on the collection object
-//   client.close();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.get("/", (req, res) => {
+  // res.send('hello i am working')
+  res.sendFile(__dirname + "/index.html");
 });
 
+client.connect((err) => {
+  const collection = client.db("organicdb").collection("products");
 
-app.listen(3000)
+  app.post("/addProduct", (req, res) => {
+    const product = req.body;
+    console.log(product);
+  });
+
+  //   const product = {name:"modhu",price:25, quantity:20 }
+  //   collection.insertOne(product)
+  //   .then(result =>{
+  // console.log("one product added")
+  //   })
+
+  // perform actions on the collection object
+  //   client.close();
+});
+
+app.listen(3000);
