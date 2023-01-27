95c0243c8974a6a5465fa44e40e49ea942106238
suparansharma <suparansharmap@gmail.com>
2023-01-27 20:07:26 +0600

    all about eshop_dashboard

--- eshop_dashboard_front/src/components/SearchBar.js
+++ eshop_dashboard_front/src/components/SearchBar.js
@@ -1,55 +1,61 @@
-import React, { useState } from 'react'
-import Table from 'react-bootstrap/Table';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
+import { Link } from "react-router-dom";
 
 const SearchBar = () => {
-    const [data, setData] = useState([])
-    const search = async (key) => {
-        let result = await fetch(`http://localhost:8000/api/search/${key}`);
-        result = await result.json();
-        setData(result);
+  const [data, setData] = useState([]);
+  const search = async (key) => {
+    let result = await fetch(`http://localhost:8000/api/search/${key}`);
+    result = await result.json();
+    setData(result);
 
-        console.log(key)
-    }
+    console.log(key);
+  };
 
-    return (
-        <>
-            <div >
-                <div className="col-sm-8 offset-sm-2" >
-                    <h1 className='text-center'>SearchBar</h1>
-                    <input type="text" name="" onChange={(e) => search(e.target.value)} className='form-control' id="" />
-                </div>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Name</th>
-                            <th>Price</th>
-                            <th>Description</th>
-                            <th>Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            data.map((item) =>
-                                <tr>
-                                    <td>{item.id}</td>
-                                    <td>{item.name}</td>
-                                    <td>{item.price}</td>
-                                    <td>{item.description}</td>
-                                    <td><img style={{ width: 100 }} src={"http://localhost:8000/" + item.file_path} /></td>
+  return (
+    <>
+      <div>
+        <div className="col-sm-8 offset-sm-2">
+          <h1 className="text-center">SearchBar</h1>
+          <input
+            type="text"
+            name=""
+            onChange={(e) => search(e.target.value)}
+            className="form-control"
+            id=""
+          />
+        <br />
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Name</th>
+              <th>Price</th>
+              <th>Description</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data.map((item) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+                <td>{item.price}</td>
+                <td>{item.description}</td>
+                <td>
+                  <img
+                    style={{ width: 100 }}
+                    src={"http://localhost:8000/" + item.file_path}
+                  />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+        </div>
+      </div>
+    </>
+  );
+};
 
-                                </tr>
-
-                            )
-                        }
-                    </tbody>
-
-
-                </Table>
-            </div>
-        </>
-    )
-}
-
-export default SearchBar
+export default SearchBar;
