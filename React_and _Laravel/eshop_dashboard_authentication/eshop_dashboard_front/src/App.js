10d00d5cd4e246e058785b947a12fa0eb3f6f44c
suparansharma <suparansharmap@gmail.com>
2023-03-05 00:35:51 +0600

    registration from

--- React_and _Laravel/eshop_dashboard_authentication/eshop_dashboard_front/src/App.js
+++ React_and _Laravel/eshop_dashboard_authentication/eshop_dashboard_front/src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Component/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Login from './Component/Login/Login';
+import Register from './Component/Register/Register';
 
 function App() {
   console.log("first")
   return (
-   <>
-   <Header/>
-   <h2>hello</h2>
-   </>
+    <>
+      <Header />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ React_and _Laravel/eshop_dashboard_authentication/eshop_dashboard_front/src/Component/Register/Register.js
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+const Register = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const signUp = async () => {
+    let items = { name, email, password };
+    const url = "http://localhost:8000/api/register";
+    let result = await fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(items),
+      headers: {
+        'Content-Type': 'application/json',
+        "Accept": 'application/json'
+      }
+    })
+
+    result = await result.json()
+    localStorage.setItem("user-info",JSON.stringify(result));
+    // navigate('/add')
+
+
+    console.log("result", result);
+
+    // console.log(items)
+  }
+  return (
+    <div className='row justify-content-center pt-5'>
+      <div className="col-sm-6">
+        <div className="card p-4">
+
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Name</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              aria-describedby="emailHelp"
+              placeholder="Enter name"
+              onChange={(e) => setName(e.target.value)}
+            />
+
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="exampleInputEmail1">Email address</label>
+            <input
+              type="email"
+              className="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Enter email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+          </div>
+          <div className="form-group">
+            <label htmlFor="exampleInputPassword1">Password</label>
+            <input
+              type="password"
+              className="form-control"
+              id="exampleInputPassword1"
+              placeholder="Password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="btn btn-primary mt-3" onClick={signUp}>Submit</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Register
