49e6c76c41ea19c7a53bd01bd0b9967216228315
suparansharma <suparansharmap@gmail.com>
2023-03-13 00:43:25 +0600

    project set up

---
+++ React_and _Laravel/bus_ticket_counter/bus_ticket_api/app/Http/Controllers/UserController/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers\UserController;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
+
+class UserController
+{
+    public function __construct()
+    {
+    }
+
+    public function go(Request $request, Response $response)
+    {
+
+    }
+}
---
+++ React_and _Laravel/bus_ticket_counter/bus_ticket_api/routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+
+Route::post('login', [AuthController::class,'login']);
+Route::post('register',[AuthController::class,'register']);
+
+Route::group(['middleware' => 'api',], function ($router) {
+
+    Route::post('logout', [AuthController::class,'logout']);
+    Route::post('refresh', [AuthController::class,'refresh']);
+    Route::post('me', [AuthController::class,'me']);
+
+    Route::post('user', [UserController::class, 'go']);
+
+});
